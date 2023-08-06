39ae71cdf6930a248da4e735a050ff151a5a0f31
Salman-165324 <salmansadi165324@gmail.com>
2023-08-06 20:29:18 +0600

    Rearraged the import statements and added the environment variables in the vercel to resolve server connetion issue after deployment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+require('dotenv').config(); 
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-
 const port = process.env.PORT || 5000;
+
 const app = express();
+const cors = require('cors');
+
 
 // middleware 
-
 app.use(cors());
 app.use(express.json())
 
